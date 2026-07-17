e055fb7424d160610fe963eb225c9db4d7f861a0
imran <shimranirn@gmail.com>
2026-07-17 10:09:41 +0530

    modified the variables and details

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,6 +4,6 @@ terraform {
     resource_group_name  = "CredApp"
     storage_account_name = "credprojectstate"
     container_name       = "statefile"
-    key                  = "credpays4.terraform.tfstate"
+    key                  = "credpays1.terraform.tfstate"
   }
 }
